b49725f05c194d74acff2e74e56db17be44c7f77
luisNavasArg <lpipnavas@gmail.com>
2022-03-24 18:00:56 -0300

    Listo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react'
+import Data from './db/data.json'
+let categorias ={all:"Todos",cats:"Gatos",dogs:"Perros"}
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      choice:categorias.all,
+      todos:[]
+    }
+    
+    // this.cambiar=this.cambiar.bind(this)
+  }
+  cambiar=(i)=>{
+     
+    this.setState({
+      choice:i.target.value
+    })
+    
+  }
+  componentDidMount(){
+    this.setState({
+      todos:Data
+    })
+ 
+  }
+  render(){
+    // let gatos = Data[0].cats;
+    // let perros = Data[1].dogs;
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <div className='col-8'>
+      {this.state.choice}
+      {this.state.choice === "Todos"
+      ?
+        this.state.todos.map((i,index)=><li key={index}>{i.name}</li>)
+      : Data.filter((element)=>element.type === this.state.choice)
+      .map((element,index)=>(
+        <li key={index+2}>{element.name}</li>
+      ))
+      }
+      </div>
+      <div className='col-4'>
+      <button onClick={this.cambiar} value="dogs">{categorias.dogs}</button>
+      <button onClick={this.cambiar} value="cats">{categorias.cats}</button>
+      </div>
+     
     </div>
   );
+}
 }
 
 export default App;
